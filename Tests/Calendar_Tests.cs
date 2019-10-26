d1aa7912797484b93a1befadf7bbb1762c4712bf
Nick Randolph <nick@builttoroam.com>
2019-10-27 08:17:11 +1100

    Added Calendar sample page with viewmodel Added Calendar shared partial class Added Calendar android partial class Added Calendar (other platforms) partial class Added IsSupported to all platforms Added Calendar test for netstandard IsSupported

---
+++ Tests/Calendar_Tests.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xunit;
+
+namespace Tests
+{
+    public class Calendar_Tests
+    {
+        [Fact]
+        public async Task Calendar_IsSupported_Fail_On_NetStandard() =>
+            await Assert.ThrowsAsync<NotImplementedInReferenceAssemblyException>(() => Task.FromResult(Calendar.IsSupported));
+    }
+}
