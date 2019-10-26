7fddb2a2e059abd710655aca495b270c82dc56f6
Nick Randolph <nick@builttoroam.com>
2019-10-27 08:17:11 +1100

    Added Calendar sample page with viewmodel Added Calendar shared partial class Added Calendar android partial class Added Calendar (other platforms) partial class Added IsSupported to all platforms Added Calendar test for netstandard IsSupported

---
+++ Samples/Samples/ViewModel/CalendarViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Samples.ViewModel
+{
+    class CalendarViewModel : BaseViewModel
+    {
+        public CalendarViewModel()
+        {
+        }
+    }
+}
