3e078d1506c89a611713459e177597ad0db8684c
Scott <scott@builttoroam.com>
2019-10-31 14:39:36 +1100

    Calendar Events able to be displayed for all or a specified calendar

--- Xamarin.Essentials/Calendar/Calendar.shared.cs
+++ Xamarin.Essentials/Calendar/Calendar.shared.cs
@@ -5,16 +5,14 @@ namespace Xamarin.Essentials
 {
     public static partial class Calendar
     {
-        public static bool IsSupported
-            => PlatformIsSupported;
+        public static bool IsSupported => PlatformIsSupported;
 
-        public static Task<IReadOnlyList<ICalendar>> GetCalendarsAsync()
-            => PlatformGetCalendarsAsync();
+        public static Task<IReadOnlyList<ICalendar>> GetCalendarsAsync() => PlatformGetCalendarsAsync();
 
-        public static Task RequestCalendarReadAccess()
-            => PlatformRequestCalendarReadAccess();
+        public static Task<IReadOnlyList<IEvent>> GetEventsAsync(string calendarId = "") => PlatformGetEventsAsync(calendarId);
 
-        public static Task RequestCalendarWriteAccess()
-            => PlatformRequestCalendarWriteAccess();
+        public static Task RequestCalendarReadAccess() => PlatformRequestCalendarReadAccess();
+
+        public static Task RequestCalendarWriteAccess() => PlatformRequestCalendarWriteAccess();
     }
 }
