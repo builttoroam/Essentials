9f71610228ae88d5794ba8e8b907470db19eebf1
Scott <scott@builttoroam.com>
2019-10-31 15:55:06 +1100

    Get Calendars working in sample for both all or specificed calendar on one page.

--- Samples/Samples/View/CalendarPage.xaml.cs
+++ Samples/Samples/View/CalendarPage.xaml.cs
@@ -1,57 +1,15 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace Samples.View
 {
-    public partial class CalendarPage : TabbedPage
+    public partial class CalendarPage : BasePage
     {
         public CalendarPage()
         {
             InitializeComponent();
-        }
-
-        public async void OnClickCalendarSpecificEvents(object sender, ItemTappedEventArgs e)
-        {
-            if (e != null && e.Item is DeviceCalendar)
-            {
-                var eventListView = new ObservableCollection<IEvent>();
-                var events = await Calendar.GetEventsAsync((e.Item as ICalendar).Id);
-                foreach (var evnt in events)
-                {
-                    eventListView.Add(evnt);
-                }
-                var listView = new ListView
-                {
-                    ItemsSource = eventListView,
-                    ItemTemplate = new DataTemplate(() =>
-                    {
-                        var title = new Label();
-                        title.SetBinding(Label.TextProperty, "Title");
-
-                        return new ViewCell
-                        {
-                            View = new StackLayout
-                            {
-                                Children =
-                                {
-                                    title
-                                }
-                            }
-                        };
-                    })
-                };
-                var detailPage = new ContentPage
-                {
-                    BindingContext = e.Item as ICalendar,
-                    Content = new StackLayout()
-                    {
-                        Children = { listView }
-                    }
-                };
-                CalendarList.SelectedItem = null;
-                await Navigation.PushModalAsync(detailPage);
-            }
         }
     }
 }
