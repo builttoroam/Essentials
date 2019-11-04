06fe4ff6a3da3a6895eaa5e8eb4d0e78b88affdd
Scott <scott@builttoroam.com>
2019-11-04 17:25:55 +1100

    Add backend data call to be able to create new event when provided with a calendar id and required fields.

--- Samples/Samples/View/CalendarPage.xaml.cs
+++ Samples/Samples/View/CalendarPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using Samples.ViewModel;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 
@@ -18,7 +19,18 @@ namespace Samples.View
                 return;
 
             var modal = new CalendarEventPage();
-            modal.BindingContext = e.Item as IEvent;
+            modal.BindingContext = e.Item as Event;
+            Navigation.PushModalAsync(modal);
+        }
+
+        void OnAddEventButtonClicked(object sender, EventArgs e)
+        {
+            var modal = new CalendarEventAddPage();
+
+            if (!(SelectedCalendar.SelectedItem is ICalendar tst) || string.IsNullOrEmpty(tst.Id))
+                return;
+
+            modal.BindingContext = new CalendarEventAddViewModel(tst.Id, tst.Name);
             Navigation.PushModalAsync(modal);
         }
     }
