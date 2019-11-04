4167d827d590aa36c3b2a4d249ca67061631977a
Scott <scott@builttoroam.com>
2019-11-04 15:12:59 +1100

    Added Github repository reference to class pulled from outside source.

--- Samples/Samples/Behaviors/BehaviorBase.cs
+++ Samples/Samples/Behaviors/BehaviorBase.cs
@@ -4,6 +4,11 @@
 using System;
 using Xamarin.Forms;
 
+/*
+ * This code is a slightly modified version of the Xamarin-Forms-sample/Behaviors code provided here:
+ * https://github.com/xamarin/xamarin-forms-samples/tree/master/Behaviors/EventToCommandBehavior/EventToCommandBehavior/Behaviors
+ */
+
 namespace Samples.Behaviors
 {
     public class BehaviorBase<T> : Behavior<T>
