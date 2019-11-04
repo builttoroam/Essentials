62c8f2fb66de82cf11f658e61869d6d28f936d7f
Scott <scott@builttoroam.com>
2019-11-04 17:25:02 +1100

    Added new CalendarEventAddPage as a modal to enter data required to create an event

--- Samples/Samples/ViewModel/CalendarEventAddViewModel.cs
+++ Samples/Samples/ViewModel/CalendarEventAddViewModel.cs
@@ -21,100 +21,27 @@ namespace Samples.ViewModel
 
         public string CalendarName { get; set; }
 
-        string eventTitle;
-
-        public string EventTitle
-        {
-            get => eventTitle;
-            set
-            {
-                if (SetProperty(ref eventTitle, value))
-                {
-                    OnPropertyChanged(nameof(CanCreateEvent));
-                }
-            }
-        }
-
-        public bool CanCreateEvent => !string.IsNullOrWhiteSpace(EventTitle) && ((EndDate.Date == StartDate.Date && (EndTime > StartTime || AllDay)) || EndDate.Date > StartDate.Date);
+        public string EventTitle { get; set; }
 
         public string Description { get; set; }
 
         public string EventLocation { get; set; }
 
-        bool allDay;
+        public bool AllDay { get; set; }
 
-        public bool AllDay
-        {
-            get => allDay;
-            set
-            {
-                if (SetProperty(ref allDay, value))
-                {
-                    OnPropertyChanged(nameof(CanCreateEvent));
-                }
-            }
-        }
+        public DateTime StartDate { get; set; } = DateTime.Now;
 
-        DateTime startDate = DateTime.Now.Date;
+        public TimeSpan StartTime { get; set; }
 
-        public DateTime StartDate
-        {
-            get => startDate;
-            set
-            {
-                if (SetProperty(ref startDate, value))
-                {
-                    OnPropertyChanged(nameof(CanCreateEvent));
-                }
-            }
-        }
+        public DateTime EndDate { get; set; } = DateTime.Now;
 
-        TimeSpan startTime = DateTime.Now.AddHours(1).TimeOfDay.RoundToNearestMinutes(30);
-
-        public TimeSpan StartTime
-        {
-            get => startTime;
-            set
-            {
-                if (SetProperty(ref startTime, value))
-                {
-                    OnPropertyChanged(nameof(CanCreateEvent));
-                }
-            }
-        }
-
-        DateTime endDate = DateTime.Now.Date;
-
-        public DateTime EndDate
-        {
-            get => endDate;
-            set
-            {
-                if (SetProperty(ref endDate, value))
-                {
-                    OnPropertyChanged(nameof(CanCreateEvent));
-                }
-            }
-        }
-
-        TimeSpan endTime = DateTime.Now.AddHours(2).TimeOfDay.RoundToNearestMinutes(30);
-
-        public TimeSpan EndTime
-        {
-            get => endTime;
-            set
-            {
-                if (SetProperty(ref endTime, value))
-                {
-                    OnPropertyChanged(nameof(CanCreateEvent));
-                }
-            }
-        }
+        public TimeSpan EndTime { get; set; }
 
         public ICommand CreateEvent { get; }
 
         async void CreateCalendarEvent()
         {
+            await Calendar.RequestCalendarWriteAccess();
             var startDto = new DateTimeOffset(StartDate + StartTime);
             var endDto = new DateTimeOffset(EndDate + EndTime);
             var newEvent = new Event()
@@ -123,14 +50,11 @@ namespace Samples.ViewModel
                 Title = EventTitle,
                 AllDay = AllDay,
                 Description = Description,
-                Location = EventLocation,
                 Start = startDto.ToUnixTimeMilliseconds(),
                 End = endDto.ToUnixTimeMilliseconds()
             };
 
             var eventId = await Calendar.CreateCalendarEvent(newEvent);
-
-            await DisplayAlertAsync("Created event id: " + eventId);
         }
     }
 }
