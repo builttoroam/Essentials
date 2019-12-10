c70df7ad1e7e0fa3dff3852d2159be62b67b7a02
Scott <scott@builttoroam.com>
2019-12-10 14:39:54 +1100

    Adding On Delete Button

--- Xamarin.Essentials/Calendar/Calendar.ios.cs
+++ Xamarin.Essentials/Calendar/Calendar.ios.cs
@@ -137,5 +137,16 @@ namespace Xamarin.Essentials
             }
             return attendees.AsReadOnly();
         }
+
+        static async Task<string> PlatformDeleteCalendarEventById(string eventId, string calendarId)
+        {
+            await Permissions.RequireAsync(PermissionType.CalendarWrite);
+
+            if (CalendarRequest.Instance.SaveEvent(evnt, EKSpan.ThisEvent, true, out var error))
+            {
+                return evnt.EventIdentifier;
+            }
+            throw new Exception(error.DebugDescription);
+        }
     }
 }
