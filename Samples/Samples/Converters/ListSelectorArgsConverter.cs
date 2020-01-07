ece8d56ca49ae6c68502c92bb641201b775b528e
Scott <scott@builttoroam.com>
2020-01-07 16:49:32 +1100

    Using new UWP recurrence rule model
    (with a readonly version existing to either allow us to view rules we do not process the power to create, or to revert back to at a later stage)
    
    the iOS and Android is commented out at this stage as they will most likely require reworks once we have a clear vision going forward.
    
    the selection of days of the week works for selection but not initilization will be looking to convert to a list of checkboxes tomorrow if I am unable to solve it within the first hour.

---
+++ Samples/Samples/Converters/ListSelectorArgsConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using Samples.ViewModel;
+using Xamarin.Forms;
+
+namespace Samples.Converters
+{
+    public class ListSelectorArgsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var eventArgs = value as ObservableCollection<TestDumbReference>;
+
+            if (eventArgs == null)
+                return value;
+
+            return eventArgs;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
