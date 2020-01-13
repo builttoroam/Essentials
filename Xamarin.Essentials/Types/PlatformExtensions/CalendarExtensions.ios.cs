23f60507e80f5b0c8444ea4cacea73e669452964
Scott <scott@builttoroam.com>
2020-01-13 11:20:37 +1100

    Android fixes, plus timezone fixes for android + iOS

--- Xamarin.Essentials/Types/PlatformExtensions/CalendarExtensions.ios.cs
+++ Xamarin.Essentials/Types/PlatformExtensions/CalendarExtensions.ios.cs
@@ -14,7 +14,5 @@ namespace Xamarin.Essentials
         static long ToEpochTime(this NSDate date) => (long)Math.Floor((Math.Abs(NSDate.FromTimeIntervalSince1970(0).SecondsSinceReferenceDate) + date.SecondsSinceReferenceDate) * 1000);
 
         public static NSDate ToNSDate(this DateTimeOffset date) => NSDate.FromTimeIntervalSinceReferenceDate((date.UtcDateTime - iosNSDateTimeSystemZeroPoint).TotalSeconds).AddSeconds(date.Offset.TotalSeconds);
-
-        public static DateTimeOffset ToDateTimeOffset(this NSDate date) => DateTimeOffset.FromUnixTimeMilliseconds(date.ToEpochTime());
     }
 }
